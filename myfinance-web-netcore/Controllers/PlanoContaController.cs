3fc9ba8adbfb19374d01638238317e4b2c414aaf
Jéssica Medeiros <jetmedeiros@gmail.com>
2024-08-07 16:20:06 -0300

    Tela de crud de plano de contas ok

---
+++ myfinance-web-netcore/Controllers/PlanoContaController.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using myfinance_web_dotnet_service.Interfaces;
+using myfinance_web_netcore.Models;
+
+namespace myfinance_web_netcore.Controllers
+{
+    [Route("[controller]")]
+    public class PlanoContaController : Controller
+    {
+        private readonly ILogger<PlanoContaController> _logger;
+        private readonly IPlanoContaService _planoContaService;
+
+        public PlanoContaController(
+            ILogger<PlanoContaController> logger,
+            IPlanoContaService planoContaService)
+        {
+            _logger = logger;
+            _planoContaService = planoContaService;
+        }
+
+        [HttpGet]
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            var listaPlanosContas = _planoContaService.ListarRegistros();
+            List<PlanoContaModel> listaPlanoContaModel = new List<PlanoContaModel>();
+
+            foreach (var item in listaPlanosContas)
+            {
+                var itemPlanoConta = new PlanoContaModel()
+                {
+                    Id = item.Id,
+                    Descricao = item.Descricao,
+                    Tipo = item.Tipo
+                };
+
+                listaPlanoContaModel.Add(itemPlanoConta);
+            }
+
+            ViewBag.ListaPlanoConta = listaPlanoContaModel;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+
+
+    }
+}
